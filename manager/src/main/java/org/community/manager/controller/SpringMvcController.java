9e329db931a5238288a23b4e287d1e420caea40c
frodoking <awangyun8@gmail.com>
2015-01-22 20:57:02 +0800

    add example with freemarker

--- manager/src/main/java/org/community/manager/controller/SpringMvcController.java
+++ manager/src/main/java/org/community/manager/controller/SpringMvcController.java
@@ -1,6 +1,8 @@
 package org.community.manager.controller;
 
+import org.community.manager.model.StudentLogin;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -19,5 +21,12 @@ public class SpringMvcController {
         ModelAndView mv = new ModelAndView("welcome");
         mv.addObject("name", "My First Spring Mvc");
         return mv;
+    }
+
+    @RequestMapping(value="/login", method=RequestMethod.GET)
+    public String login(Model model) {
+        StudentLogin studentLogin = new StudentLogin();
+        model.addAttribute("studentLogin", studentLogin);
+        return "login";
     }
 }
